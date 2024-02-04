bb6dffd84606a769608f6668f9a14e36c80255e2
mannetroll <torbjorn.sjogren@gmail.com>
2024-02-04 21:31:46 +0100

    log4j2.enable.threadlocals=false

--- src/test/java/com/mannetroll/metrics/logger/JsonLayoutTest.java
+++ src/test/java/com/mannetroll/metrics/logger/JsonLayoutTest.java
@@ -1,5 +1,6 @@
 package com.mannetroll.metrics.logger;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,9 +11,13 @@ import org.junit.Test;
 public class JsonLayoutTest {
 	private static final Logger LOG = LogManager.getLogger(JsonLayoutTest.class);
 
+	//
+	// -Dlog4j2.enable.threadlocals=false
+	//
+	
 	@Test
-	public void test() {
-		System.setProperty("log4j2.enable.threadlocals", "false");
+	public void test() throws ReflectiveOperationException {
+		JsonLayoutTest.updateEnv("log4j2.enable.threadlocals", "false");
 		Map<String, Object> tmp = new HashMap<String, Object>();
 		tmp.put("a_string", "hello");
 		tmp.put("a_number", 11L);
@@ -21,4 +26,12 @@ public class JsonLayoutTest {
 		LOG.info(tmp);
 	}
 
+	@SuppressWarnings({ "unchecked" })
+	  public static void updateEnv(String name, String val) throws ReflectiveOperationException {
+	    Map<String, String> env = System.getenv();
+	    Field field = env.getClass().getDeclaredField("m");
+	    field.setAccessible(true);
+	    ((Map<String, String>) field.get(env)).put(name, val);
+	  }	
+	
 }
