daa3ccf95e704d5244a002e516c47eefd02a2f3b
mannetroll <torbjorn.sjogren@gmail.com>
2024-03-20 21:12:10 +0100

    Removed: osInfo

--- src/main/java/com/mannetroll/metrics/logger/MetricJsonLayout.java
+++ src/main/java/com/mannetroll/metrics/logger/MetricJsonLayout.java
@@ -21,18 +21,16 @@ public class MetricJsonLayout extends AbstractStringLayout {
 
 	private final boolean locationInfo;
 	private final boolean b3TracingInfo;
-	private final boolean osInfo;
 	private final String application;
 	private final String namespace;
 	private final boolean sort;
 	private final MetricEventDataBuilder metricsData = new MetricEventDataBuilder();
 
-	protected MetricJsonLayout(boolean locationInfo, boolean osInfo, String application, String namespace,
-			Charset charset, boolean b3TracingInfo, boolean sort) {
+	protected MetricJsonLayout(boolean locationInfo, String application, String namespace, Charset charset,
+			boolean b3TracingInfo, boolean sort) {
 		super(charset);
 		this.locationInfo = locationInfo;
 		this.b3TracingInfo = b3TracingInfo;
-		this.osInfo = osInfo;
 		this.application = application;
 		this.namespace = namespace;
 		this.sort = sort;
@@ -40,19 +38,18 @@ public class MetricJsonLayout extends AbstractStringLayout {
 
 	@PluginFactory
 	public static MetricJsonLayout createLayout(@PluginAttribute(value = "locationInfo") boolean locationInfo,
-			@PluginAttribute(value = "osInfo", defaultBoolean = true) Boolean osInfo,
 			@PluginAttribute(value = "application") String application, @PluginAttribute("namespace") String namespace,
 			@PluginAttribute(value = "charset", defaultString = "UTF-8") Charset charset,
 			@PluginAttribute(value = "b3TracingInfo") boolean b3TracingInfo,
 			@PluginAttribute(value = "sort") boolean sort) {
-		return new MetricJsonLayout(locationInfo, osInfo, application, namespace, charset, b3TracingInfo, sort);
+		return new MetricJsonLayout(locationInfo, application, namespace, charset, b3TracingInfo, sort);
 	}
 
 	@Override
 	public String toSerializable(LogEvent event) {
 		try {
-			Map<String, Object> logData = metricsData.toJson(event, application, namespace, locationInfo, osInfo,
-					b3TracingInfo, sort);
+			Map<String, Object> logData = metricsData.toJson(event, application, namespace, locationInfo, b3TracingInfo,
+					sort);
 
 			return JsonUtil.toJson(logData);
 		} catch (Exception e) {
--- src/main/java/com/mannetroll/metrics/util/MetricEventDataBuilder.java
+++ src/main/java/com/mannetroll/metrics/util/MetricEventDataBuilder.java
@@ -3,7 +3,6 @@ package com.mannetroll.metrics.util;
 import static com.mannetroll.metrics.util.MDCUtils.getMDCString;
 import static com.mannetroll.metrics.util.MDCUtils.safePutValue;
 
-import java.lang.management.ManagementFactory;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -13,26 +12,16 @@ import com.codahale.metrics.Snapshot;
 import com.mannetroll.metrics.codahale.AppenderMetricsManager;
 import com.mannetroll.metrics.codahale.AppenderTimer;
 import com.mannetroll.metrics.helper.Constants;
-import com.sun.management.OperatingSystemMXBean;
 
 /**
  * @author mannetroll
  */
 public class MetricEventDataBuilder {
 	private static final String METRICS = "metrics";
-	private static OperatingSystemMXBean osmxbean = null;
-
 	private final LoggingEventDataBuilder metricEventDataBuilder = new LoggingEventDataBuilder();
 
-	static {
-		try {
-			osmxbean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-		} catch (Exception ignored) {
-		}
-	}
-
 	public Map<String, Object> toJson(LogEvent event, String application, String namespace, boolean locationInfo,
-			boolean osInfo, boolean b3TracingInfo, boolean sort) {
+			boolean b3TracingInfo, boolean sort) {
 		Map<String, Object> map = metricEventDataBuilder.getMap(event, application, namespace, locationInfo,
 				b3TracingInfo, sort);
 		//
@@ -47,19 +36,18 @@ public class MetricEventDataBuilder {
 			if (nanotimeStr != null) {
 				long nanotime = Long.parseLong(nanotimeStr);
 				long responsetime_ns = timer.timestartstop(nanotime, System.nanoTime());
-				addToJSON(map, timer, responsetime_ns, event, osInfo);
+				addToJSON(map, timer, responsetime_ns, event);
 			} else if (responsetime_msStr != null) {
 				long responsetime_ms = Long.parseLong(responsetime_msStr);
 				long responsetime_ns = timer.timestartstop(0, TimeUnit.MILLISECONDS.toNanos(responsetime_ms));
-				addToJSON(map, timer, responsetime_ns, event, osInfo);
+				addToJSON(map, timer, responsetime_ns, event);
 			}
 		}
 
 		return map;
 	}
 
-	private void addToJSON(Map<String, Object> tmp, AppenderTimer timer, long responsetime_ns, LogEvent event,
-			boolean osInfo) {
+	private void addToJSON(Map<String, Object> tmp, AppenderTimer timer, long responsetime_ns, LogEvent event) {
 		tmp.put(LogKeys.TYPE, METRICS);
 		tmp.put(LogKeys.STATUS, toInt(getMDCString(Constants.RESPONSE_STATUS, event)));
 		safePutValue(tmp, LogKeys.DOMAIN, getMDCString(Constants.DOMAIN, event));
@@ -88,14 +76,6 @@ public class MetricEventDataBuilder {
 		tmp.put(LogKeys._999THPERCENTILE, TimeUnit.NANOSECONDS.toMillis((long) snapshot.get999thPercentile()));
 		// Runtime
 		tmp.put("memfree", Runtime.getRuntime().freeMemory());
-		// OperatingSystemMXBean
-		if (MetricEventDataBuilder.osmxbean != null && osInfo) {
-			tmp.put("freephysicalmemorysize", osmxbean.getFreePhysicalMemorySize());
-			tmp.put("processcpuload", osmxbean.getProcessCpuLoad());
-			tmp.put("processcputime", osmxbean.getProcessCpuTime());
-			tmp.put("systemcpuload", osmxbean.getSystemCpuLoad());
-			tmp.put("systemloadaverage", osmxbean.getSystemLoadAverage());
-		}
 	}
 
 	private int toInt(String intStr) {
